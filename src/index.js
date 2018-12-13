27f23e3092f77e50644b662b0c19827e963da9d7
lihui <lihui@yunbs.com>
2018-12-13 14:31:56 +0800

    开始路由项目

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import {
+    HashRouter as Router,//容器
+    Route//一条路由
+}from 'react-router-dom';
+function Home(){
+    return <h1>Home</h1>
+}
+function User(){
+    return <h1>User</h1>
+}
+function Profile(){
+    return <h1>Profile</h1>
+}
+ReactDOM.render(
+    <Router>
+        <div>
+            <Route path="/home" component={Home}></Route>
+            <Route path="/user" component={User}></Route>
+            <Route path="/profile" component={Profile}></Route>
+        </div>
+    </Router>,document.querySelector('#root')
+)
